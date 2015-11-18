fdab3952be1387daf03fae93b258382ad2812fa6
Aleli Evangelista <hello@alelievangelista.com>
2015-11-18 18:05:32 -0500

    Initial commit

---
+++ app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -0,0 +1,290 @@
+package it.jaschke.alexandria;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import it.jaschke.alexandria.data.AlexandriaContract;
+import it.jaschke.alexandria.services.BookService;
+import it.jaschke.alexandria.services.CaptureActivityAnyOrientation;
+import it.jaschke.alexandria.services.DownloadImage;
+
+
+public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String TAG = "INTENT_TO_SCAN_ACTIVITY";
+    private EditText ean;
+    private final int LOADER_ID = 1;
+    private View rootView;
+    private final String EAN_CONTENT="eanContent";
+    private static final String SCAN_FORMAT = "scanFormat";
+    private static final String SCAN_CONTENTS = "scanContents";
+
+    private String mScanFormat = "Format:";
+    private String mScanContents = "Contents:";
+
+
+
+    public AddBook(){
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(ean!=null) {
+            outState.putString(EAN_CONTENT, ean.getText().toString());
+        }
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(R.layout.fragment_add_book, container, false);
+        ean = (EditText) rootView.findViewById(R.id.ean);
+
+        ean.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //no need
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //User has started typing - warn them that network unavailable
+                if(!isNetworkAvailable()){
+                    Context context = getActivity();
+                    CharSequence text = "Network connection unavailable";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    return; //Nope out
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String ean =s.toString();
+                //catch isbn10 numbers
+                if(ean.length()==10 && !ean.startsWith("978")){
+                    ean="978"+ean;
+                }
+                if(ean.length()<13){
+                    clearFields();
+                    return;
+                }
+
+                Log.d("afterTextChanged", "Starting book intent");
+
+                //Once we have an ISBN, start a book intent
+                Intent bookIntent = new Intent(getActivity(), BookService.class);
+                bookIntent.putExtra(BookService.EAN, ean);
+                bookIntent.setAction(BookService.FETCH_BOOK);
+                getActivity().startService(bookIntent);
+                AddBook.this.restartLoader();
+            }
+        });
+
+        rootView.findViewById(R.id.scan_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!isNetworkAvailable()){
+                    Context context = getActivity();
+                    CharSequence text = "Network connection unavailable";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                    return; //Nope out
+                }
+
+                //Initiate book scanner
+                IntentIntegrator integrator = new IntentIntegrator(getActivity());
+                IntentIntegrator fragIntegrator = integrator.forSupportFragment(AddBook.this);
+                fragIntegrator.setPrompt("Scan a book barcode");
+                fragIntegrator.setCaptureActivity(CaptureActivityAnyOrientation.class);
+                fragIntegrator.setOrientationLocked(false);
+                fragIntegrator.initiateScan();
+
+            }
+        });
+
+        rootView.findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ean.setText("");
+                String e = ean.getText().toString();
+
+                //Once we have an ISBN, start a book intent
+                Intent bookIntent = new Intent(getActivity(), BookService.class);
+                bookIntent.putExtra(BookService.EAN, e);
+                bookIntent.setAction(BookService.ADD_BOOK);
+                getActivity().startService(bookIntent);
+                AddBook.this.restartLoader();
+            }
+        });
+
+        rootView.findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bookIntent = new Intent(getActivity(), BookService.class);
+                bookIntent.putExtra(BookService.EAN, ean.getText().toString());
+                bookIntent.setAction(BookService.DELETE_BOOK);
+                getActivity().startService(bookIntent);
+                ean.setText("");
+            }
+        });
+
+        if(savedInstanceState!=null){
+            ean.setText(savedInstanceState.getString(EAN_CONTENT));
+            ean.setHint("");
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        switch (requestCode) {
+            case IntentIntegrator.REQUEST_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+
+                    // Parsing bar code reader result
+                    IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+                    String format = result.getFormatName();
+                    String content = result.getContents();
+                    parseScannedBarcode(format, content);
+                }
+                break;
+        }
+    }
+
+    /**
+     * Triggers a search based on the barcode scanned
+     * @param format The barcode format type
+     * @param content The barcode content
+     */
+    private void parseScannedBarcode(String format, String content){
+
+        //This currently immediately adds to favourites o__O
+        if(format.equals("EAN_13") && content.startsWith("978") && content.length()==13){
+
+            //Set the code
+            ean.setText(content);
+
+            //Initialize book search intent
+            Intent bookIntentScan = new Intent(getActivity(), BookService.class);
+            bookIntentScan.putExtra(BookService.EAN, content);
+            bookIntentScan.setAction(BookService.FETCH_BOOK);
+            getActivity().startService(bookIntentScan);
+            AddBook.this.restartLoader();
+        }
+    }
+
+    //Check network connectivity
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+    private void restartLoader(){
+        getLoaderManager().restartLoader(LOADER_ID, null, this);
+    }
+
+    @Override
+    public android.support.v4.content.Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if(ean.getText().length()==0){
+            return null;
+        }
+        String eanStr= ean.getText().toString();
+        if(eanStr.length()==10 && !eanStr.startsWith("978")){
+            eanStr="978"+eanStr;
+        }
+        return new CursorLoader(
+                getActivity(),
+                AlexandriaContract.BookEntry.buildFullBookUri(Long.parseLong(eanStr)),
+                null,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor data) {
+        if (!data.moveToFirst()) {
+            return;
+        }
+
+        String bookTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
+        ((TextView) rootView.findViewById(R.id.bookTitle)).setText(bookTitle);
+
+        String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
+        ((TextView) rootView.findViewById(R.id.bookSubTitle)).setText(bookSubTitle);
+
+        String authors = data.getString(data.getColumnIndex(AlexandriaContract.AuthorEntry.AUTHOR));
+        String[] authorsArr = authors.split(",");
+        ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
+        ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",","\n"));
+        String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
+        if(Patterns.WEB_URL.matcher(imgUrl).matches()){
+            new DownloadImage((ImageView) rootView.findViewById(R.id.bookCover)).execute(imgUrl);
+            rootView.findViewById(R.id.bookCover).setVisibility(View.VISIBLE);
+        }
+
+        String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
+        ((TextView) rootView.findViewById(R.id.categories)).setText(categories);
+
+        rootView.findViewById(R.id.save_button).setVisibility(View.VISIBLE);
+        rootView.findViewById(R.id.delete_button).setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
+
+    }
+
+    private void clearFields(){
+        ((TextView) rootView.findViewById(R.id.bookTitle)).setText("");
+        ((TextView) rootView.findViewById(R.id.bookSubTitle)).setText("");
+        ((TextView) rootView.findViewById(R.id.authors)).setText("");
+        ((TextView) rootView.findViewById(R.id.categories)).setText("");
+        rootView.findViewById(R.id.bookCover).setVisibility(View.INVISIBLE);
+        rootView.findViewById(R.id.save_button).setVisibility(View.INVISIBLE);
+        rootView.findViewById(R.id.delete_button).setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        activity.setTitle(R.string.scan);
+    }
+}
